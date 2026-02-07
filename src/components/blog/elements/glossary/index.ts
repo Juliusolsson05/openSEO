72cf2dce163f1f033f415a06b4823f0dc0846774
Julius Olsson <julius.olsson05@gmail.com>
2026-02-07 12:53:11 +0100

    fix(ui): add missing glossary SVG icon

--- src/components/blog/elements/glossary/index.ts
+++ src/components/blog/elements/glossary/index.ts
@@ -4,6 +4,7 @@ import { GlossaryPreview } from './GlossaryPreview'
 import { GlossaryLoading } from './GlossaryLoading'
 import { glossaryEditSchema } from './edit_schema'
 import { glossaryExample } from './example'
+import GlossaryIcon from './Icon'
 
 registerElement('glossary', {
   component: Glossary,
@@ -11,6 +12,7 @@ registerElement('glossary', {
   loading: GlossaryLoading,
   editSchema: glossaryEditSchema,
   example: glossaryExample,
+  icon: GlossaryIcon,
 })
 
 export { Glossary, GlossaryPreview, GlossaryLoading, glossaryEditSchema, glossaryExample }
