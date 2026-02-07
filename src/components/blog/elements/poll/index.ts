27e6bf5c4d8e219d1c4c76fc57b3981252c396fa
Julius Olsson <julius.olsson05@gmail.com>
2026-02-07 12:51:04 +0100

    feat(ui): add missing example data for element previews

--- src/components/blog/elements/poll/index.ts
+++ src/components/blog/elements/poll/index.ts
@@ -2,13 +2,16 @@ import { registerElement } from '../registry'
 import { Poll } from './Poll'
 import { PollPreview } from './PollPreview'
 import { PollLoading } from './PollLoading'
+import { pollExample } from './example'
 
 export { Poll } from './Poll'
 export { PollPreview } from './PollPreview'
 export { PollLoading } from './PollLoading'
+export { pollExample } from './example'
 
 registerElement('poll', {
   component: Poll,
   preview: PollPreview,
   loading: PollLoading,
+  example: pollExample,
 })
